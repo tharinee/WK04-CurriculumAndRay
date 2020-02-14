9fbee3996a9cd125fe05111d6ebbd40c9614b80d
55 <55>
2020-02-14 10:45:01 +0700

    1

---
+++ Assets/Scripts/PenguinAcademy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+
+public class PenguinAcademy : Academy
+{
+    public float FishSpeed { get; private set; }
+
+    public float FeedRadius { get; private set; }
+
+    public override void InitializeAcademy()
+    {
+    	FishSpeed = 0f;
+    	FeedRadius =0f;
+
+    	FloatProperties.RegisterCallback("fish_speed", f =>
+    	{
+    		FishSpeed = f;
+    	});
+
+    	FloatProperties.RegisterCallback("feed_radius", f =>
+    	{
+    		FeedRadius = f;
+    	});
+    }
+}
---
+++ Assets/Scripts/PenguinArea.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+using TMPro;
+
+public class PenguinArea : Area
+{
+	public PenguinAgent penguinAgent;
+	public GameObject penguinBaby;
+	public TextMeshPro cumulativeRewardText;
+	public Fish fishPrefab;
+
+	private PenguinAcademy penguinAcademy;
+	private List<GameObject> fishList;
+
+	public override void ResetArea()
+	{
+		RemoveAllFish();
+		PlacePenguin();
+		PlaceBaby();
+		SpawnFish(4, penguinAcademy.FishSpeed);
+	}
+
+	public void RemoveSpecificFish(GameObject fishObject)
+	{
+		fishList.Remove(fishObject);
+		Destroy(fishObject);
+	}
+
+	public int FishRemaining
+	{
+		get { return fishList.Count; }
+	}
+
+	public static Vector3 ChooseRandomPosition(Vector3 center, float minAngle, float maxAngle, float minRadius, float maxRadius)
+	{
+		float radius = minRadius;
+		float angle = minAngle;
+
+		if(maxRadius > minRadius)
+		{
+			radius = UnityEngine.Random.Range(minRadius, maxRadius);
+		}
+		if(maxAngle > minAngle)
+		{
+			angle = UnityEngine.Random.Range(minAngle, maxAngle);
+		}
+
+		return center + Quaternion.Euler(0f, angle, 0f) * Vector3.forward * radius;
+	}
+
+	private void RemoveAllFish()
+	{
+		if(fishList != null)
+		{
+			for(int i = 0; i < fishList.Count; i++)
+			{
+				if(fishList[i] != null)
+				{
+					Destroy(fishList[i]);
+				}
+			}
+		}
+		fishList = new List<GameObject>();
+	}
+
+	private void PlacePenguin()
+	{
+		Rigidbody rigidbody = penguinAgent.GetComponent<Rigidbody>();
+		rigidbody.velocity = Vector3.zero;
+		rigidbody.angularVelocity = Vector3.zero;
+		penguinAgent.transform.position = ChooseRandomPosition(transform.position, 0f, 360f, 0f, 9f) + Vector3.up * .5f;
+		penguinAgent.transform.rotation = Quaternion.Euler(0f, UnityEngine.Random.Range(0f, 360f), 0f);
+	}
+
+	private void PlaceBaby()
+	{
+		Rigidbody rigidbody = penguinBaby.GetComponent<Rigidbody>();
+		rigidbody.velocity = Vector3.zero;
+		rigidbody.angularVelocity = Vector3.zero;
+		penguinBaby.transform.position = ChooseRandomPosition(transform.position, -45f, 45f, 4f, 9f) + Vector3.up * .5f;
+		penguinBaby.transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+	}
+
+	private void SpawnFish(int count, float fishSpeed)
+	{
+		for(int i = 0; i < count; i++)
+		{
+			//Spawn and place the fish
+			GameObject fishObject = Instantiate<GameObject>(fishPrefab.gameObject);
+			fishObject.transform.position = ChooseRandomPosition(transform.position, 100f, 260f, 2f, 13f) + Vector3.up * .5f;
+			fishObject.transform.rotation = Quaternion.Euler(0f, UnityEngine.Random.Range(0f, 360f), 0f);
+
+        	// Set the fish's parent to this area's transform
+			fishObject.transform.SetParent(transform);
+
+        	// Keep track of the fish
+			fishList.Add(fishObject);
+
+       		// Set the fish speed
+			fishObject.GetComponent<Fish>().fishSpeed = fishSpeed;
+		}
+	}
+
+	private void Start()
+	{
+		penguinAcademy = FindObjectOfType<PenguinAcademy>();
+		ResetArea();
+	}
+
+	private void Update()
+	{
+    	// Update the cumulative reward text
+		cumulativeRewardText.text = penguinAgent.GetCumulativeReward().ToString("0.00");
+	}
+}
