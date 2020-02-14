02a5daa17b4651e685f2fab8fe1b02b669ed70d9
Tharinee_pukboong <32834492+tharinee@users.noreply.github.com>
2020-02-14 12:01:42 +0700

    1

---
+++ Assets/Scripts/PenguinAcademy.cs
@@ -0,0 +1,26 @@
+﻿using UntiyEngine;
+using MLAgents;
+
+public class PenguinAcademy : Academy
+{
+
+	public float FishSpeed {get; private set;}
+	
+	public float FeedRadius {get; private set;}
+
+public override void IntializeAcademy(){
+
+	FishSpeed = 0f;
+	FeedRadius = 0f;
+
+	FloatProperties.RegisteCallback("fish_speed", f =>
+	{
+		FishSpeed = f;
+		});
+	FloatProperties.RegisterCallback("feed_radius", f =>
+	{
+		FeedRadius = f;
+		});
+
+}
+}
---
+++ Assets/Scripts/PenguinArea.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+using TMPro;
+
+public class PenguinArea : Area
+{
+    public PenguinAgent penguinAgent;
+    public GameObject penguinBaby;
+    public TextMeshPro cumulativeRewardText;
+    public Fish fishPrefeb;
+
+    private PenguinAcademy penguinAcademy;
+    private List<GameObject> fishList;
+
+    public override void ResetArea(){
+
+    	RemoveAllFis();
+    	PlacePenguin();
+		PlaceBaby();
+		SpawnFish(4, penguinAcademy.FishSpeed);
+    }
+    public void RemoveSpecificFish(GameObject fishObject){
+    	fishList.Remove(fishObject);
+    	Destroy(fishObject);
+    }
+    public int FishRemaining{
+    	get { retun fishList.coun; }
+    }
+}
